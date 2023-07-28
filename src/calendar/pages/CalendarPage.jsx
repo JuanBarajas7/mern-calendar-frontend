fce3c01c23c35817534a65e578d25f134d207841
juan_barajas7@hotmail.com <juan_barajas7@hotmail.com>
2023-07-28 10:18:02 -0700

    adición de los demas archivos

---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,108 @@
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { CalendarEvent, CalendarModal, FabAddNew, FabDelete, NavBar } from '../';
+import { localizer, getMessagesES } from '../../helpers';
+import { useEffect, useState } from 'react';
+import { useUiStore } from '../../hooks/useUiStore';
+import { useCalendarStore } from '../../hooks/useCalendarStore';
+import { useAuthStore } from '../../hooks';
+
+
+
+
+export const CalendarPage = () => {
+
+  const {user} = useAuthStore();
+  const {openDateModal}=useUiStore();
+  const {events, setActiveEvent, activeEvent, startLoadingEvents}=useCalendarStore();
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView')||'week');
+
+  const eventStyleGetter=(event, start, end, isSelected)=>{
+
+    const isMyEvent=(user.uid===event.user._id)||(user.uid===event.user.uid);
+
+    const bgColor = isMyEvent
+                    ?
+                      // (activeEvent!==null && event.id===activeEvent.id)
+                      (event.id===activeEvent?.id)
+                      ?'#0b3378'
+                      :'#347CF7'
+                    :
+                      // (activeEvent!==null && event.id===activeEvent.id)
+                      (event.id===activeEvent?.id)
+                      ?'#252e33'
+                      :'#465660';
+                    
+                    
+
+    const style={
+      // backgroundColor: '#347CF7',
+      // backgroundColor: (activeEvent!==null && event.id===activeEvent.id)?'#0b3378':'#347CF7',
+      backgroundColor: bgColor,
+      borderRadius:'0px',
+      opacity:0.8,
+      color:'white'
+    }
+    return{
+      style
+    }
+  }
+
+
+  const onDoubleClick=(event)=>{
+    // console.log({doubleClick:event});
+    openDateModal();
+  }
+
+  const onSelect=(event)=>{
+    //  console.log({click:event});
+    setActiveEvent(event);
+  }
+
+  const onViewChanged=(event)=>{
+    localStorage.setItem('lastView',event);
+    setLastView(event);
+  }
+  const onSelectSlotChanged=(event)=>{
+    if(activeEvent!==null){
+      setActiveEvent(null);
+    }
+  }
+
+  useEffect(() => {
+    startLoadingEvents();
+  }, [])
+  
+
+  return (
+    <>
+      <NavBar/>    
+
+      <Calendar
+        selectable
+        culture='es'
+        localizer={localizer}
+        events={events}
+        defaultView={lastView}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event:CalendarEvent
+        }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectSlot={onSelectSlotChanged}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
+        
+        
+      />
+      <CalendarModal/>
+      <FabAddNew/>
+      <FabDelete/>
+    </>
+  )
+}
